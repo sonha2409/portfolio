d0a168b793abfe51d3f589cebfc4606d1e69ce0a
Son Ha <sonhathai.2409@gmail.com>
2023-06-28 06:30:57 -0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import React from 'react'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Son's portfolio</title>
+      </Head>
+      <h1>Roger Federer</h1>
+    </div>
+  )
+}
+export default Home;
