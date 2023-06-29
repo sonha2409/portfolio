b7d2dd0a91d0ec068fd93fb8f902115eba64bf81
Son Ha <sonhathai.2409@gmail.com>
2023-06-28 22:19:21 -0400

    Add photo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from './BackgroundCircles';
+import Image from 'next/image';
 
 type Props = {}
 
@@ -15,10 +16,17 @@ export default function Hero({}: Props) {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7ABBA" />
-      </h1>
+      <div className=' relative mt-20 md:mb-0 flex-shrink-0 w-56 h-56  md:w-64 md:h-96 xl:w-[400px] xl:h-[500px]'>
+                    <Image src={require('../assets/son-rounded.png')} className=' rounded-full h-32 w-32 mx-auto object-cover' layout='fill' objectFit='cover' alt={''} />
+      </div>
+      <div>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Engineer</h2>
+        <h1>
+          <span className='mr-2'>{text}</span>
+          <Cursor cursorColor="#F7ABBA" />
+        </h1>
+      </div>
+
     </div>
   )
 }
