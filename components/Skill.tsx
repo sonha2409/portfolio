97a102d3e9f69e1300d46e54cbfe3b5368233157
Son Ha <sonhathai.2409@gmail.com>
2023-06-29 00:33:56 -0400

    Finalizing Skill

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -20,10 +20,16 @@ function Skill({ directionLeft }: Props) {
           opacity: 1, x: 0
         }}
         src="https://logos-world.net/wp-content/uploads/2022/07/Java-Logo.png"
-        className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32
+        className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 md:w-28 md:h-28
         filter group-hover:grayscale transition duration-300 ease-in-out"
       />
-      
+      <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out
+      group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+        <div className="flex items-center justify-center h-full">
+          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+        </div>
+      </div>
+
         
 
     </div>
