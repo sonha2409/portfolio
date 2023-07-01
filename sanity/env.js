c8645f6cb5ed7ae9033d526a7513730fbc5d727b
Son Ha <sonhathai.2409@gmail.com>
2023-07-01 15:54:17 -0400

    Initial Sanity set up

---
+++ sanity/env.js
@@ -0,0 +1,6 @@
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-07-01'
+
+export const dataset = "production";
+export const projectId = "6bu7qhz8";
+export const useCdn = false
---
+++ sanity/schema.js
@@ -0,0 +1,9 @@
+
+import pageInfo from './schemas/pageInfo'
+import skill from './schemas/skill'
+import social from './schemas/social'
+import experience from './schemas/experience'
+import project from './schemas/project'
+export const schema = {
+  types: [pageInfo, skill, social, experience, project],
+}
